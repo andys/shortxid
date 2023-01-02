e6be1667f2f3fa29f9f43c69d3c8e79863f4a9f7
Andrew Snow <andrew@modulus.org>
2023-01-02 18:49:50 +1100

    format

--- shortxid.go
+++ shortxid.go
@@ -1,41 +1,42 @@
 package shortxid
 
 import (
-  "sync/atomic"
-  "time"
-	"golang.org/x/exp/constraints"
+	"sync/atomic"
+	"time"
+
 	"github.com/jxskiss/base62"
+	"golang.org/x/exp/constraints"
 )
 
 const (
-  epochMillis = uint64(1672491600) * 1000  // starts 2023 and ends 2057 (40 bits)
-  encoding = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	epochMillis = uint64(1672491600) * 1000 // starts 2023 and ends 2057 (40 bits)
+	encoding    = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 )
 
 type Generator struct {
-  id uint64
-  counter uint64
-  globalPrepend string
-  encoder *base62.Encoding
-  TimeFunc func() uint64
+	id            uint64
+	counter       uint64
+	globalPrepend string
+	encoder       *base62.Encoding
+	TimeFunc      func() uint64
 }
 
 func NewGenerator[I constraints.Integer](id I, globalPrepend string) *Generator {
-  return &Generator{
-    id: (uint64(id) & 0xff) << (8 * 2),
-    counter: 0,
-    globalPrepend: globalPrepend,
-    encoder: base62.NewEncoding(encoding),
-    TimeFunc: func() uint64 {
-      return uint64(time.Now().UnixMilli()) - epochMillis
-    },
-  }
+	return &Generator{
+		id:            (uint64(id) & 0xff) << (8 * 2),
+		counter:       0,
+		globalPrepend: globalPrepend,
+		encoder:       base62.NewEncoding(encoding),
+		TimeFunc: func() uint64 {
+			return uint64(time.Now().UnixMilli()) - epochMillis
+		},
+	}
 }
 
 func (g *Generator) NewID(prepend string) string {
-  now := g.TimeFunc() 
-  ctr := atomic.AddUint64(&g.counter, 1)
+	now := g.TimeFunc()
+	ctr := atomic.AddUint64(&g.counter, 1)
 
-  newIDint := ((now & 0xffffffffff) << (8 * 3)) | (ctr & 0xffff) | (g.id)
-  return g.globalPrepend + prepend + string(g.encoder.FormatUint(newIDint))
+	newIDint := ((now & 0xffffffffff) << (8 * 3)) | (ctr & 0xffff) | (g.id)
+	return g.globalPrepend + prepend + string(g.encoder.FormatUint(newIDint))
 }
--- shortxid_test.go
+++ shortxid_test.go
@@ -1,35 +1,35 @@
 package shortxid
 
 import (
-  "testing"
-  "strings"
+	"strings"
+	"testing"
 )
 
 func TestShortxid_NewID_Prepend(t *testing.T) {
-  gen := NewGenerator(0, "GGG")
-  
-  if gen == nil {
-    t.Fatal("failed to create new generator")
-  }
+	gen := NewGenerator(0, "GGG")
 
-  result := gen.NewID("III")
-  
-  if !strings.HasPrefix(result, "GGGIII") {
-    t.Fatal("failed to generate with prefix")
-  }
-  
-  result2 := gen.NewID("III")
-  if result == result2 {
-    t.Fatal("failed to generator unique id")
-  }
+	if gen == nil {
+		t.Fatal("failed to create new generator")
+	}
+
+	result := gen.NewID("III")
+
+	if !strings.HasPrefix(result, "GGGIII") {
+		t.Fatal("failed to generate with prefix")
+	}
+
+	result2 := gen.NewID("III")
+	if result == result2 {
+		t.Fatal("failed to generator unique id")
+	}
 }
 
 func TestShortxid_NewID_Expected(t *testing.T) {
-  gen := NewGenerator(12345, "")
-  gen.TimeFunc = func() uint64 { return 1234567890 }
-  
-  result := gen.NewID("")
-  if result != "1WrZ9RdWC1" {
-    t.Fatal("new ID did not match expected: " + result)
-  }
+	gen := NewGenerator(12345, "")
+	gen.TimeFunc = func() uint64 { return 1234567890 }
+
+	result := gen.NewID("")
+	if result != "1WrZ9RdWC1" {
+		t.Fatal("new ID did not match expected: " + result)
+	}
 }
